b518b611df5c371833fa48ddad8198dab9ab9863
Valik <valik3201@gmail.com>
2024-02-16 23:46:49 +0100

    style: add responsive styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,9 @@ import { BookUserIcon } from 'lucide-react';
 function App() {
   return (
     <div className="container mx-auto md:max-w-xl flex flex-col gap-4 p-8">
-      <div className="flex flex-row gap-4 justify-center items-center pb-8">
+      <div className="flex flex-row gap-4 justify-center items-center md:pb-8">
         <BookUserIcon className="w-12 h-12 text-blue-600" />
-        <h1 className="text-6xl font-medium">
+        <h1 className="text-3xl md:text-6xl font-medium">
           Phone<span className="text-blue-600">Book</span>
         </h1>
       </div>
